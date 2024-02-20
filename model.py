9c749f7b23438d3801afe78123b20838b393cd34
Chris Mangum <csmangum@gmail.com>
2024-02-20 16:27:17 -0700

    Refactor Rule30CNN to AutomataCNN and add save and load methods

--- model.py
+++ model.py
@@ -2,7 +2,36 @@ import torch
 import torch.nn as nn
 
 
-class Rule30CNN(nn.Module):
+class AutomataCNN(nn.Module):
+    """
+    A convolutional neural network for learning 1D cellular automata rules.
+
+    The model takes as input a tensor of shape (batch_size, 1, num_cells) and
+    returns a tensor of shape (batch_size, num_cells) containing the next state
+    of the cellular automaton.
+
+    The model consists of a single convolutional layer followed by a fully
+    connected layer.
+
+    Attributes
+    ----------
+    num_cells : int
+        The number of cells in each row of the cellular automaton.
+    conv1 : nn.Conv1d
+        The first convolutional layer.
+    fc1 : nn.Linear
+        The first fully connected layer.
+
+    Methods
+    -------
+    forward(x: torch.Tensor) -> torch.Tensor
+        Forward pass of the Rule30CNN model.
+    save(path: str) -> None
+        Save the model to file.
+    load(path: str) -> None
+        Load the model from file.
+    """
+
     def __init__(self, num_cells: int = 101) -> None:
         """
         Initialize the Rule30CNN model.
@@ -12,7 +41,7 @@ class Rule30CNN(nn.Module):
         num_cells : int
             The number of cells in each row of the cellular automaton.
         """
-        super(Rule30CNN, self).__init__()
+        super(AutomataCNN, self).__init__()
         self.num_cells = num_cells
         self.conv1 = nn.Conv1d(1, 10, kernel_size=3, padding=1)
         self.fc1 = nn.Linear(10 * num_cells, num_cells)
@@ -35,3 +64,20 @@ class Rule30CNN(nn.Module):
         x = x.view(-1, 10 * self.num_cells)
         x = torch.sigmoid(self.fc1(x))
         return x
+
+    def save(self, path: str) -> None:
+        """
+        Save the model to file.
+
+        Parameters
+        ----------
+        path : str
+            The path to save the model to.
+        """
+        torch.save(self.state_dict(), path)
+
+    def load(self, path: str) -> None:
+        """
+        Load the model from file.
+        """
+        self.load_state_dict(torch.load(path))
