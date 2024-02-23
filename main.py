34903ada0150873a94365b7af88981faa3cf32f7
Chris Mangum <csmangum@gmail.com>
2024-02-22 21:08:18 -0700

    Refactor main.py: Import modules and update function names

--- main.py
+++ main.py
@@ -1,70 +1,32 @@
-import os
-import shutil
-import time
-
-from matplotlib import pyplot as plt
-
+from animations import evolution, training_evolution
+from charts import plot_automata, rule_states
 from learn import Learn
 from settings import *
-
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
-
-def make_dir(path):
-    try:
-        if os.path.exists(path):
-            shutil.rmtree(path)
-        else:
-            os.makedirs(path)
-    except PermissionError as e:
-        print(f"PermissionError encountered: {e}. Retrying...")
-        time.sleep(1)  # Wait a bit in case the file is temporarily locked
-        if os.path.exists(path):  # Check again to see if the directory still exists
-            shutil.rmtree(path)
-        else:
-            os.makedirs(path)
+from util import make_dir
 
 
-def plot_automata(rule_number, automata, path):
-    plt.figure(figsize=(10, 10))
-    plt.imshow(automata, cmap="binary", interpolation="nearest")
-    # plt.title(f"Real Cellular Automata Rule {rule_number}")
-    plt.axis("off")
-    plt.savefig(path + f"real_automata.png", bbox_inches="tight")
-    plt.close()
+def main() -> None:
+    """
+    Learn a cellular automaton and visualize the results.
+    """
+
+    make_dir(PATH)
+    learn = Learn(
+        rule_number=RULE_NUMBER,
+        num_cells=NUM_CELLS,
+        num_generations=GENERATIONS,
+        learning_rate=LEARNING_RATE,
+        training_size=TRAINING_SIZE,
+        epochs=EPOCHS,
+        path=PATH,
+    )
+    real_automata = learn.automata.generate(100)
+    plot_automata(RULE_NUMBER, real_automata, PATH)
+    learn.train()
+    evolution(real_automata, RULE_NUMBER)
+    training_evolution(RULE_NUMBER)
+    rule_states(RULE_NUMBER)
 
 
-# for i in range(0, 256):
-#     rule_number = i
-#     path = f"results/rule_{rule_number}/"
-#     make_dir(path)
-#     learn = Learn(
-#         rule_number=rule_number,
-#         num_cells=NUM_CELLS,
-#         num_generations=GENERATIONS,
-#         learning_rate=LEARNING_RATE,
-#         training_size=TRAINING_SIZE,
-#         epochs=EPOCHS,
-#         path=path,
-#     )
-#     real_automata = learn.automata.generate(100)
-#     plot_automata(rule_number, real_automata, path)
-#     learn.train()
-
-# make_dir(PATH)
-# learn = Learn(
-#     rule_number=RULE_NUMBER,
-#     num_cells=NUM_CELLS,
-#     num_generations=GENERATIONS,
-#     learning_rate=LEARNING_RATE,
-#     training_size=TRAINING_SIZE,
-#     epochs=EPOCHS,
-#     path=PATH,
-# )
-# real_automata = learn.automata.generate(100)
-# plot_automata(RULE_NUMBER, real_automata, PATH)
-# learn.train()
-
-from animations.training import training_evolution
-
-# training_evolution(73)
+if __name__ == "__main__":
+    main()
