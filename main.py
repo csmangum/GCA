3efe58e4f89e18522d086327e18e4d296a2de66d
Chris Mangum <csmangum@gmail.com>
2024-02-25 21:45:33 -0700

    Update main.py and settings.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from animations import evolution, training_evolution
-from charts import plot_automata, rule_states
-from learn import Learn
+# from animations import evolution, training_evolution
+# from charts import plot_automata, rule_states
+# from learn import Learn
+from learning.all_rules import Learn
 from settings import *
 from util import make_dir
 
@@ -10,9 +11,8 @@ def main() -> None:
     Learn a cellular automaton and visualize the results.
     """
 
-    make_dir(PATH)
+    # make_dir(PATH)
     learn = Learn(
-        rule_number=RULE_NUMBER,
         num_cells=NUM_CELLS,
         num_generations=GENERATIONS,
         learning_rate=LEARNING_RATE,
@@ -20,12 +20,12 @@ def main() -> None:
         epochs=EPOCHS,
         path=PATH,
     )
-    real_automata = learn.automata.generate(100)
-    plot_automata(RULE_NUMBER, real_automata, PATH)
+    # real_automata = learn.automata.generate(100)
+    # plot_automata(RULE_NUMBER, real_automata, PATH)
     learn.train()
-    evolution(real_automata, RULE_NUMBER)
-    training_evolution(RULE_NUMBER)
-    rule_states(RULE_NUMBER)
+    # evolution(real_automata, RULE_NUMBER)
+    # training_evolution(RULE_NUMBER)
+    # rule_states(RULE_NUMBER)
 
 
 if __name__ == "__main__":
