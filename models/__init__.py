2f5772bfc4dd43dfa798461f92ecf8b98e999a70
Chris Mangum <csmangum@gmail.com>
2024-05-03 15:28:09 -0700

    Refactor ModelFactory to support dynamic instantiation of model classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,22 @@
 from models.cnn import AutomataCNN as AutomataCNN
 from models.cnn import GeneralAutomataCNN as GeneralAutomataCNN
+from models.evolution import ArtificialEvolution as ArtificialEvolution
+from models.simple import SimpleSequentialNetwork as SimpleSequentialNetwork
+
+
+class ModelFactory:
+    """
+    Factory class for creating models on the fly
+    """
+
+    def __init__(self, model_class: type) -> None:
+        """
+        Parameters
+        ----------
+        model_class : type
+            The model class to instantiate
+        """
+        self.model_class = model_class
+
+    def __call__(self) -> type:
+        return self.model_class()
