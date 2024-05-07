20127c9376cb45f372810b48d6ba72abf0a06635
Chris Mangum <csmangum@gmail.com>
2024-05-06 18:28:01 -0700

    Refactor ModelFactory to remove unnecessary code and improve readability

--- models/__init__.py
+++ models/__init__.py
@@ -3,24 +3,3 @@ from models.cnn import GeneralAutomataCNN as GeneralAutomataCNN
 from models.evolution import ArtificialEvolution as ArtificialEvolution
 from models.simple import SimpleSequentialNetwork as SimpleSequentialNetwork
 from models.transformer import SimpleTransformer as SimpleTransformer
-
-
-class ModelFactory:
-    """
-    Factory class for creating models on the fly
-    """
-
-    def __init__(self, model_class: type, arguments: dict) -> None:
-        """
-        Parameters
-        ----------
-        model_class : type
-            The model class to instantiate
-        arguments : dict
-            The arguments to pass to the model class
-        """
-        self.model_class = model_class
-        self.arguments = arguments
-
-    def __call__(self) -> type:
-        return self.model_class(**self.arguments)
